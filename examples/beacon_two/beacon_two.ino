2b23a91fcc0c0610ca6e06cc791b6202e48009ec
Denis Bogdanov <bogdenoff@MacBook-Pro-Denis.local>
2017-05-25 15:59:03 +0300

    Voltage changed
    Voltage changed

--- examples/beacon_two/beacon_two.ino
+++ examples/beacon_two/beacon_two.ino
@@ -10,15 +10,15 @@ void setup()
 void loop()
 {
   //appling PWM signal for changing voltage for 85 (1,66V)
-  analogWrite(LED_PIN,85);
+  analogWrite(LED_PIN,35);
   //applying delay 250ms
   delay(250);
   //changing voltage for 170 (3,33V)
-  analogWrite(LED_PIN,170);
+  analogWrite(LED_PIN,120);
   //applying delay 250ms
   delay(250);
   //changing voltage for 255 (5V)
-  analogWrite(LED_PIN,255);
+  analogWrite(LED_PIN,35);
   //applying delay 250ms
   delay(250);
   
