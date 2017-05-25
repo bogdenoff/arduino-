a6377424fbb873b3c0c04b86cd07783a800cc23f
Denis Bogdanov <bogdenoff@MacBook-Pro-Denis.local>
2017-05-25 15:35:36 +0300

    Script for beacon
    First testing of Arduino

---
+++ beacon_one.cpp
@@ -0,0 +1,23 @@
+void setup()
+{
+	//enabling output mode for pin 13
+	pinMode(13, OUTPUT);
+}
+
+void loop()
+{
+	//applying high signal to pin 13, which means 5 voltage. This will make the beacon glow
+	digitalWrite (13, HIGH);
+
+	//applying delay 100ms
+	delay(100);
+
+	//applying low signal to pin 13, which means 0 voltage. This will stop glowing of beacon
+	digitalWrite (13, LOW);
+
+	//applying delay 900ms
+	delay(900);
+
+	//after delay loop will start again
+	//in total beacon will glow for 100ms+900ms=1s
+}
